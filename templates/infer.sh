762c41e2bff844898f548587f6296084673ea669
Jacopo Grassi <jacopo.grassi@polito.it>
2025-11-21 14:56:17 +0100

    Simplify output file path in infer.sh
    Removes redundant OUTDIR variable and directory creation. The output file is now written directly to the input directory path.

--- templates/infer.sh
+++ templates/infer.sh
@@ -11,10 +11,7 @@ MEMBER_NUM="${MEMBER#fc}"
 
 INDIR="${HPCROOTDIR}/DATA/${START_DATE}/${MEMBER}"
 INFILE="${INDIR}/${START_ISO}_r${MEMBER_NUM}.nc"
-
-OUTDIR="${HPCROOTDIR}/DATA/${START_DATE}/${MEMBER}"
-mkdir -p "${OUTDIR}"
-OUTFILE="${OUTDIR}/${START_ISO}_r${MEMBER_NUM}_infer.nc"
+OUTFILE="${INDIR}/${START_ISO}_r${MEMBER_NUM}_infer.nc"
 
 # Activate Python virtual environment
 source /home/jgrassi/code/neuralGCM/venv/bin/activate
