49b12f32cdde8a2f542736d33a5530867ee12d20
Jacopo Grassi <jacopo.grassi@polito.it>
2025-11-21 14:42:16 +0100

    Update argument name in infer.sh script
    Changed the argument from --outer_steps to --num_steps when calling infer.py to match expected parameter naming.

--- templates/infer.sh
+++ templates/infer.sh
@@ -22,4 +22,4 @@ source /home/jgrassi/code/neuralGCM/venv/bin/activate
 python ${HPCROOTDIR}/git_project/runscript/infer.py \
   --input_path "${INFILE}" \
   --output_path "${OUTFILE}" \
-  --outer_steps 10
+  --num_steps 10
